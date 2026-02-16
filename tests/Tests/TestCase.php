4e4c3236468031400933685e5b984da36c27d52e
IgnacioCastro0713 <ignacio.g3@hotmail.com>
2026-02-15 21:59:27 -0600

    Fix: Explicitly define static property \$latestResponse in TestCase for Orchestr      Testbench compatibility

--- tests/Tests/TestCase.php
+++ tests/Tests/TestCase.php
@@ -7,6 +7,13 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
+    /**
+     * Fix for "Access to undeclared static property" error in Orchestra Testbench
+     * when used with certain versions of Pest/PHPUnit.
+     * @var mixed
+     */
+    public static $latestResponse = null;
+
     protected function getPackageProviders($app): array
     {
         $app['config']->set('mediator.handler_paths', __DIR__ . '/../Unit');
